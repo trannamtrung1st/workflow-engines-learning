3cc67d9230c9afe31cfbca90f4ea83cd74abfc23
Trung Tran <trung.tran@yokogawa.com>
2024-06-07 11:14:59 +0700

    Check in code

--- WELearning.ConsoleApp.Testing/ValueObjects/EntryValueObject.cs
+++ WELearning.ConsoleApp.Testing/ValueObjects/EntryValueObject.cs
@@ -5,7 +5,7 @@ using WELearning.Core.FunctionBlocks.Models.Design;
 
 namespace WELearning.ConsoleApp.Testing.ValueObjects;
 
-public abstract class EntryValueObject : RawValueObject
+public class EntryValueObject : RawValueObject
 {
     protected readonly EntryEntity _entity;
 
@@ -13,60 +13,13 @@ public abstract class EntryValueObject : RawValueObject
     {
         _entity = entity;
         EntryKey = _entity.Key;
+        Value = _entity.Value;
     }
 
     public string EntryKey { get; }
     public override object Value { get => _entity.Value; set => base.Value = value; }
 
     protected override void SetCoreValue(object value) => _entity.Value = value;
-}
 
-public class RWEntryValueObject : EntryValueObject
-{
-    public RWEntryValueObject(Variable variable, EntryEntity entity) : base(variable, entity)
-    {
-        Value = _entity.Value;
-        // [NOTE] value is set
-    }
-
-    public override IValueObject CloneFor(Variable variable) => new RWEntryValueObject(variable, _entity);
-}
-
-public class REntryValueObject : EntryValueObject
-{
-    public REntryValueObject(Variable variable, EntryEntity entity) : base(variable, entity)
-    {
-        _valueSet.Set();
-        ValueChanged = false;
-        // [NOTE] value is set
-    }
-
-    public override object Value
-    {
-        set => throw new InvalidOperationException("Read-only!");
-    }
-
-    public override IValueObject CloneFor(Variable variable) => new REntryValueObject(variable, _entity);
-}
-
-public class WEntryValueObject : EntryValueObject
-{
-    public WEntryValueObject(Variable variable, EntryEntity entity) : base(variable, entity)
-    {
-        // [NOTE] value is not set
-    }
-
-    private object _value;
-    public override object Value
-    {
-        get => _value;
-    }
-
-    protected override void SetCoreValue(object value)
-    {
-        _value = value;
-        base.SetCoreValue(value);
-    }
-
-    public override IValueObject CloneFor(Variable variable) => new WEntryValueObject(variable, _entity);
+    public override IValueObject CloneFor(Variable variable) => new EntryValueObject(variable, _entity);
 }
