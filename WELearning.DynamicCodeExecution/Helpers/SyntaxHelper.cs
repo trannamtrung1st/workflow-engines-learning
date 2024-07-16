35278022e19754dffc8502f7a21377870fb51ef6
Trung Tran <trung.tran@yokogawa.com>
2024-07-16 16:29:00 +0700

    Add more helpers

--- WELearning.DynamicCodeExecution/Helpers/SyntaxHelper.cs
+++ WELearning.DynamicCodeExecution/Helpers/SyntaxHelper.cs
@@ -6,7 +6,7 @@ public static class SyntaxHelper
 {
     public static bool HasAsyncSyntax(string content) => content.Contains("await "); // [NOTE] just for convenience, should use other mechanism or manually set
 
-    public static string CleanFunctionSignature(string functionName)
+    public static string CleanJsFunctionSignature(string functionName)
     {
         if (string.IsNullOrEmpty(functionName))
         {
@@ -23,5 +23,37 @@ public static class SyntaxHelper
         }
 
         return cleanedName;
+    }
+
+    public static char[] GetJsVariableInvalidCharacters(string variableName)
+    {
+        if (string.IsNullOrEmpty(variableName))
+        {
+            return new char[0];
+        }
+
+        List<char> invalidCharacters = new List<char>();
+        for (int i = 0; i < variableName.Length; i++)
+        {
+            char c = variableName[i];
+            if (i == 0)
+            {
+                // The first character must be a letter, underscore, or dollar sign
+                if (!char.IsLetter(c) && c != '_' && c != '$')
+                {
+                    invalidCharacters.Add(c);
+                }
+            }
+            else
+            {
+                // Subsequent characters can include letters, digits, underscores, or dollar signs
+                if (!char.IsLetterOrDigit(c) && c != '_' && c != '$')
+                {
+                    invalidCharacters.Add(c);
+                }
+            }
+        }
+
+        return invalidCharacters.ToArray();
     }
 }
