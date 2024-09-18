6e870c001a63389e2a5a69d64fe3bce3639e8d9b
Trung Tran <trannamtrung1st@gmail.com>
2024-09-18 23:17:13 +0700

    Remove unused

--- WELearning.Core/FunctionBlocks/Constants/FunctionConstants.cs
+++ WELearning.Core/FunctionBlocks/Constants/FunctionConstants.cs
@@ -4,10 +4,6 @@ public static class BuiltInVariables
 {
     public const string THIS = "THIS";
     public const string EVENTS = "EVENTS";
-    public const string IN = "IN";
-    public const string OUT = "OUT";
-    public const string INOUT = "INOUT";
-    public const string INTERNAL = "INTERNAL";
 }
 
 public static class FunctionDefaults
